10dfcc5b28cfe2eec6f18356bba58aedc560d270
Suowei Hu <suowei.h@gmail.com>
2024-05-11 15:17:43 +1000

    Feat: add more govcms command

--- src/index.tsx
+++ src/index.tsx
@@ -23,19 +23,61 @@ export default function Command() {
     const [commands, setCommands] = useState<Command[]>(
         [
             {
+                type: "GovCMS",
+                title: "Unblock Root User + Login ",
+                description: "Using ahoy command to login into the drupal backend of the website with root user (uid=1)",
+                cmd: "admin_username=$(ahoy drush uinf --uid=1 --fields=name --format=string) && ahoy drush uublk $admin_username && ahoy login",
+                docs_href:"https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example: "![](2024-05-11T115503.jpg)",
+            },{
+                type: "GovCMS",
+                title: "Build Project",
+                description: "",
+                cmd: "ahoy up",
+                docs_href: "https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example: `\`\`\`\nup:\nusage: Build project.\ncmd: |\n    docker compose up -d "$@" &&\n    docker compose exec -T test dockerize -wait tcp://mariadb:3306 -timeout 2m &&\n    ahoy info;\n\`\`\``,
+            },{
+                type: "GovCMS",
+                title: "Build Project + Run Deployment",
+                description: "Build Project + Runs deployment commands (e.g. config import, updb, cr, set up file_stage_proxy).",
+                cmd: "ahoy up",
+                docs_href: "https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example: `\`\`\`\nbuild:\nusage: Build project.\ncmd: |\n    docker compose up -d --build "$@" &&\n    docker compose exec -T test dockerize -wait tcp://mariadb:3306 -timeout 2m &&\n    ahoy govcms-deploy && ahoy info;\n\`\`\``,
+            },{
+                type:"GovCMS",
+                title:"Pause Project",
+                description:"Pause the conatiner (do not remove database)",
+                cmd:"ahoy stop",
+                docs_href:"https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example:`\`\`\`\nstop:\n    usage: Stop Docker containers.\n    cmd: docker compose stop "$@"\n\`\`\``,
+            },{
+                type: "GovCMS",
+                title: "Delete Project",
+                description: "Delete project (CAUTION).",
+                cmd: "ahoy down",
+                docs_href: "https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example: `\`\`\`\ndown:\nusage: Delete project (CAUTION).\ncmd: |\n  if [ "$1" == "y" ]; then\n    docker compose down --volumes\n  else\n    ahoy confirm "Running this command will destroy your current site, database and build? Are you sure you didn't mean ahoy stop?" &&\n    # Run this if confirm returns true\n    docker compose down --volumes ||\n    # Run this if confirm returns false\n    echo "OK, probably a wise choice..."\n  fi\n\`\`\``,
+            },{
+                type: "GovCMS",
+                title: "Export Database",
+                description: "Use ahoy command to export mysql database to \".sql\" file.",
+                cmd: "ahoy mysql-dump $EXPORT_FILE_NAME",
+                docs_href: "https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example: `\`\`\`\nmysql-import:\n    usage: Pipe in a sql file.  \`ahoy mysql-import local.sql\`\n    cmd: |\n      if [ -e "$@" ] ; then\n        docker compose exec cli bash -c 'drush sql-drop' &&\n        docker compose exec -T cli bash -c 'drush sql-cli' < "$@"\n      else echo "Provided sql file" "$@" "does not exist"\n      fi\n\`\`\``,
+            },{
+                type: "GovCMS",
+                title: "Import Database",
+                description: "Use ahoy command to import mysql database",
+                cmd: "ahoy mysql-import $IMPORT_FILE_NAME",
+                docs_href: "https://github.com/govCMS/scaffold/blob/develop/.ahoy.yml",
+                example: `\`\`\`\nmysql-dump:\n   usage: Dump data out into a file. \`ahoy mysql-dump local.sql\`\n   cmd: docker compose exec -T cli bash -c 'drush sql-dump --ordered-dump' > "$@"\n\`\`\``,
+            },{
                 type: "Drush",
                 title: "Clear Cache",
                 description: "Clear/Rebuild all caches.",
                 cmd: "drush cache:rebuild;",
                 docs_href:"https://www.drush.org/12.x/commands/cache_rebuild/",
                 example: `\`\`\`\n> drush cache:rebuild\n> [success] Cache rebuild complete.\n\`\`\`\n\`\`\`\n> drush cr\n> [success] Cache rebuild complete.\n\`\`\``,
-            },{
-                type: "Other",
-                title: "Clear Cache (using composer drush)",
-                description: "Use drush executable file sitting inside the composer directory.",
-                cmd: "vendor/bin/drush cache:rebuild;",
-                docs_href:"https://www.drush.org/12.x/commands/cache_rebuild/",
-                example: `\`\`\`\n> vendor/bin/drush cache:rebuild\n> [success] Cache rebuild complete.\n\`\`\`\n\`\`\`\n> vendor/bin/drush cr\n> [success] Cache rebuild complete.\n\`\`\``,
             },{
                 type: "Drush",
                 title: "Debug verbose",
@@ -79,12 +121,12 @@ export default function Command() {
                 docs_href:"https://www.drush.org/12.x/commands/pm_list/",
                 example: `\`\`\`\npm-list:           list modules/themes\n--type=Module:     list modules only\n--status=enabled:  list enabled modules/theme only \n--format:          format the result data. \n-v|vv|vvv|verbose: increase the verbosity of messages \n\`\`\`\n![](2024-05-11T115202.jpg)`
             },{
-                type: "GovCMS",
-                title: "GovCMS Drupal Login (Unblock root user)",
-                description: "Using ahoy command to login into the drupal backend of the website with root user (uid=1)",
-                cmd: "admin_username=$(ahoy drush uinf --uid=1 --fields=name --format=string) && ahoy drush uublk $admin_username && ahoy login",
-                docs_href:"https://www.govcms.support/support/solutions/articles/51000005047-how-can-i-login-as-an-administrator-saas-on-my-local-development-environment-",
-                example: "![](2024-05-11T115503.jpg)",
+                type: "Other",
+                title: "Clear Cache (using composer drush)",
+                description: "Use drush executable file sitting inside the composer directory.",
+                cmd: "vendor/bin/drush cache:rebuild;",
+                docs_href:"https://www.drush.org/12.x/commands/cache_rebuild/",
+                example: `\`\`\`\n> vendor/bin/drush cache:rebuild\n> [success] Cache rebuild complete.\n\`\`\`\n\`\`\`\n> vendor/bin/drush cr\n> [success] Cache rebuild complete.\n\`\`\``,
             },
         ]
     );
@@ -92,13 +134,24 @@ export default function Command() {
 
     // helper function to get the tag of command
     function CommandTagColor(_command_:Command):Color{
-        if(_command_.type == "Drush")  {return(Color.Orange);}
-        if(_command_.type == "GovCMS") {return(Color.Purple);}
-        if(_command_.type == "Ahoy")   {return(Color.Purple);}
-        if(_command_.type == "Error")  {return(Color.Red   );}
-        if(_command_.type == "Twig")   {return(Color.Green );}
-        return(Color.Blue);
+        if(_command_.type == "Drush")  {return(Color.Orange      );}
+        if(_command_.type == "GovCMS") {return(Color.Purple      );}
+        if(_command_.type == "Ahoy")   {return(Color.Purple      );}
+        if(_command_.type == "Error")  {return(Color.Red         );}
+        if(_command_.type == "Twig")   {return(Color.Green       );}
+        if(true)                       {return(Color.Blue        );}
     }
+
+    // helper function to get the tag of command
+    function CommandTagIcon(_command_:Command):Icon{
+        if(_command_.type == "Drush")  {return(Icon.CommandSymbol);}
+        if(_command_.type == "GovCMS") {return(Icon.Hashtag      );}
+        if(_command_.type == "Ahoy")   {return(Icon.Hashtag      );}
+        if(_command_.type == "Error")  {return(Icon.Bug          );}
+        if(_command_.type == "Twig")   {return(Icon.CodeBlock    );}
+        if(true)                       {return(Icon.Circle       );}
+    }
+
 
     // component for details display of command
     function CommandDetail(_command_:Command){
@@ -121,6 +174,18 @@ export default function Command() {
                             <Detail.Metadata.Link title="Official Documentation" target={_command_.docs_href} text={_command_.docs_href} />
                         </Detail.Metadata>
                     }
+                    actions={
+                        <ActionPanel title="Actions">
+                            <Action.CopyToClipboard
+                                icon={Icon.CopyClipboard}
+                                title="Copy Command"
+                                content={_command_.cmd}/>
+                            <Action.OpenInBrowser
+                                icon={Icon.Compass}
+                                title="Open Documentation"
+                                url={_command_.docs_href}/>
+                        </ActionPanel>
+                    }
         />);
     }
 
@@ -136,7 +201,7 @@ export default function Command() {
             {commands.map(
                 (_command_, _index_) => (
                     <List.Item
-                        icon={Icon.Circle}
+                        icon={CommandTagIcon(_command_)}
                         key={_index_}
                         title={_command_.title}
                         subtitle={_command_.description}
@@ -144,7 +209,8 @@ export default function Command() {
                         actions={
                             <ActionPanel title="Actions">
                                 <Action
-                                    title="Push"
+                                    icon={Icon.Center}
+                                    title="Show Details"
                                     onAction={
                                         () => push(
                                             <CommandDetail
@@ -157,12 +223,10 @@ export default function Command() {
                                             />
                                         )
                                     } />
-                                <Action.CopyToClipboard
-                                    title="Copy command to clipboard"
-                                    content="https://github.com/raycast/extensions/pull/1"/>
                                 <Action.OpenInBrowser
-                                    title="Open official documentation"
-                                    url="https://github.com/raycast/extensions/pull/1"/>
+                                    icon={Icon.Compass}
+                                    title="Open Documentation"
+                                    url={_command_.docs_href}/>
                             </ActionPanel>
 
                         }
